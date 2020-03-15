8e78e732a5b41d79db989e534aad84e2d5bf3e9b
MinhNguyen <adsbox@icloud.com>
2020-03-15 14:21:08 -0500

    master cheerio implement

--- src/controllers/live-update.controller.ts
+++ src/controllers/live-update.controller.ts
@@ -6,36 +6,16 @@ export class LiveUpdateController {
   constructor() {}
 
   async liveUpdateStateData() {
-    let cheerio = require('cheerio');
-    let request = require('request');
+    const cheerio = require('cheerio');
+    const axios = require('axios');
 
-    const r = await function (resolve: any, reject: any) {
-      request('http://xmfan.com/guide.php',
-        function (err: any, response: any, body: any) {
-          if (err) reject(err);
-          if (response.statusCode !== 200) {
-            reject('Invalid status code: ' + response.statusCode);
-          }
-          let $ = cheerio.load(body);
-          let channelList = $('td[width=140]');
+    axios.get('https://www.dhs.wisconsin.gov/outbreaks/index.htm').then((response: any) => {
+      const $ = cheerio.load(response.data);
+      $('h5').filter(function () {
+        return this.text().trim() === 'Wisconsin COVID-19 Test Results';
+      }).next().text();
+    })
 
-          let channels = [];
-
-          for (let i = 0; i < channelList.length; i++) {
-            let t = channelList.get(i);
-            let channel = $(t).text();
-            let artistNode = $(t).next();
-            let artist = $(artistNode).text();
-            let title = $(artistNode).next().text();
-
-            console.log(channel + '-' + artist + '-' + title);
-            channels.push({channel: channel, artist: artist, title: title});
-          }
-
-          resolve(channels);
-
-        });
-    }
-    console.log('do something here')
+    console.log('Finish live update state data')
   }
 }
