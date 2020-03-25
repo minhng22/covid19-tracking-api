87d253649f69e616295c7298968a147b9cd287e4
MinhNguyen <adsbox@icloud.com>
2020-03-24 22:21:18 -0500

    make it better

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,5 @@
-export * from './county.controller';
+export * from './case-number.controller';
 export * from './live-update.controller';
 export * from './ping.controller';
 export * from './us-state-controller.controller';
-export * from './case-number.controller';
+
