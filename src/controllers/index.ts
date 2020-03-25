b144eb7147f63beea18a4de5ab9285db169c848d
MinhNguyen <adsbox@icloud.com>
2020-03-24 23:18:07 -0500

    add front end

--- src/controllers/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './case-number.controller';
-export * from './live-update.controller';
-export * from './ping.controller';
-export * from './us-state-controller.controller';
-
--- src/domain/Medical.ts
+++
@@ -1,4 +0,0 @@
-export const MedicalType = {
-  'Positive': 'POSITIVE',
-  'Death': 'DEATH'
-}
